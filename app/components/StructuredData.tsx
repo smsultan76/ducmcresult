dc69967e52791cba10e859957b3e600f95f67528
SULTANUM MOBIN <sultanum.mobin@gmail.com>
2026-09-08 11:30:50 +0600

    app structure

---
+++ app/components/StructuredData.tsx
@@ -0,0 +1,41 @@
+'use client';
+
+import { useEffect } from 'react';
+
+export default function StructuredData() {
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.type = 'application/ld+json';
+    script.innerHTML = JSON.stringify({
+      "@context": "https://schema.org",
+      "@type": "WebApplication",
+      "name": "DU CMC Result System",
+      "description": "Batch result lookup for Dhaka University Constituent Engineering College and Medical College",
+      "applicationCategory": "Educational Application",
+      "operatingSystem": "All",
+      "url": "https://ducmcresult.vercel.app",
+      "author": {
+        "@type": "Person",
+        "name": "Sultanum Mobin",
+        "url": "https://sultanum-mobin.vercel.app/"
+      },
+      "offers": {
+        "@type": "Offer",
+        "price": "0",
+        "priceCurrency": "USD"
+      },
+      "aggregateRating": {
+        "@type": "AggregateRating",
+        "ratingValue": "4.8",
+        "ratingCount": "127"
+      }
+    });
+    document.head.appendChild(script);
+
+    return () => {
+      document.head.removeChild(script);
+    };
+  }, []);
+
+  return null;
+}
